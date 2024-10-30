fe5ac1aa66e65af921a15db5f1b2aa7933119b5b
sunarko.sunarko <sunarko.sunarko@aux.gojek.com>
2024-10-30 15:53:45 +0700

    initial commit next project

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "CV Distributor App",
+  description: "Aplikasi PWA untuk CV Distributor",
+  themeColor: "#000000",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/icons/icon-192x192.png",      
+    apple: "/icons/icon-192x192.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
